f857767322e87ed529acb56beca35d936c0c78b2
Trevern G LAMONTAGNE <500002227@my.fiveislands.uwi.edu>
2024-11-03 16:11:55 -0400

    question 3 fix 2

--- Problem3.py
+++ Problem3.py
@@ -7,23 +7,26 @@ Create a function englishToSpongecase that will take a string as input and retur
 import random as r
 
 def englishToSpongecase(p):
-    scase = ""
-    uppercase = True 
-    rand = r.randint(0,1)
-    if rand == 0:
-        scase += p[0].upper()
-        uppercase = False
-    else:
-        scase += p[0].lower()
-        uppercase = True
-    for i in range(1,len(p)):
-        if uppercase:
-            scase += p[i].upper()
+    try:
+        scase = ""
+        uppercase = True 
+        rand = r.randint(0,1)
+        if rand == 0:
+            scase += p[0].upper()
             uppercase = False
         else:
-            scase += p[i].lower()
+            scase += p[0].lower()
             uppercase = True
-    return scase
+        for i in range(1,len(p)):
+            if uppercase:
+                scase += p[i].upper()
+                uppercase = False
+            else:
+                scase += p[i].lower()
+                uppercase = True
+        return scase
+    except Exception:
+        return p
 
 def main():
     text = input("Enter a string: ")
