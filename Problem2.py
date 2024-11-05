ef099e56f956811afc84b1f580c2373551b03d17
Trevern G LAMONTAGNE <500002227@my.fiveislands.uwi.edu>
2024-11-04 23:06:28 -0400

    Question2

--- Problem2.py
+++ Problem2.py
@@ -1,7 +1,6 @@
 import random
 
 intro = "Welcome to the guessing game. You have 10 tries to guess the number that I am thinking of between 1 and 30. \nEach time you make an incorrect guess, we will show it to you in the GRID OF GUESSES. \nYou may keep guessing until you are successful or until you have run out of guesses."
-Goal = random.randint(2, 29)
 grid = [['' for i in range(4)] for j in range(3)]
 
 #1
@@ -15,17 +14,16 @@ def print_grid(grid):
 
 #2
 def get_user_guess(attempt):
-    while True:
-        try:
-            guess = int(attempt)
-            if 1 < guess and guess < 30:
-                return guess
-        except ValueError:
-            print("Please enter a valid number between 1 and 30")
+    if attempt > 0:
+        guess = input("Enter your guessed NUMBER: ")
+        while guess.isdigit() == False or int(guess) < 2 or int(guess) > 29:
+            guess = input("Please enter a valid number: ")
+        return int(guess)
+    elif attempt == 0:
+        return save_score(attempt)
 
 #3
 def update_grid(grid, guess, guesses_made):
-    guesses_made += 1
     for row in grid:
         for i in range(len(row)):
             if row[i] == '':
@@ -35,38 +33,38 @@ def update_grid(grid, guess, guesses_made):
 #4
 def provide_feedback(guess, secret_number):
     if guess == secret_number:
-        return "Congrats, you have won!"
+        print("\nCongrats, you have won!") 
     elif guess < secret_number:
-        return "Your number is too small"
+        print("\nYour number is too small")
     else:
-        return "Your number is too big"
+        print("\nYour number is too big")
 
 #5
 def save_score(score):
     with open("myScores.txt", "w") as file:
-        file.write(f'Final score: {str(score)}\n')
+        file.write(f'Final score: {score}\n')
 
 #6
 def play_game():
+    Goal = random.randint(1, 30)
+    lives = 10
     grid = [['' for i in range(4)] for j in range(3)]
-    guesses_made = 0
-    print(intro)
     print_grid(grid)
-    while guesses_made < 10:
-        guess = get_user_guess(input("Enter your guessed NUMBER: "))
-        if isinstance(guess, int):
-            grid = update_grid(grid, guess, guesses_made)
-            print_grid(grid)
-            feedback = provide_feedback(guess, Goal)
-            print(feedback)
-            if feedback == "Congrats, you have won!":
-                save_score(guesses_made)
-                break
-            guesses_made += 1
-    if guesses_made == 10:
-        print(f"Out of tries. The secret number was {Goal}")
-        save_score(guesses_made)
+    print(intro)
 
+    for i in range(1,lives +1):
+        guess = get_user_guess(lives - i)
+        grid = update_grid(grid, guess, lives - i)
+        print_grid(grid)
+        if lives - i > 0:
+            provide_feedback(guess, Goal)
+        elif guess == Goal:
+            save_score(lives - i)
+            print('Congrats, you have won!')
+            break
+        else:
+            save_score(lives - i)
+            print('Sorry, you are out of guesses')
 #7
 def main():
     play_game()
